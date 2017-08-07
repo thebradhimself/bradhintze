88bb81f81b631f8de2b965adee3ad1327e68fc91
Bradley Hintze <bradley.hintze@gmail.com>
2017-08-07 16:03:11 -0600

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= vue
 //= require jquery
 //= require rails-ujs
 //= require bootstrap/dist/js/bootstrap
